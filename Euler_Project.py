2ae5eb779c6bf5c01d134fcf1a0b651700b0ae0c
Subacunogaara <75877293+Subacunogaara@users.noreply.github.com>
2021-07-23 17:13:09 +0300

    Update Euler_Project.py

--- Euler_Project.py
+++ Euler_Project.py
@@ -34,3 +34,22 @@
 #         break
 
 
+# Какое самое маленькое число делится нацело на все числа от 1 до 20?
+
+# import time
+
+# ran = 1
+# for n in range(1, 21):
+#     ran *= n
+# print(ran)
+# start = time.time()
+# for i in range(20, ran+1):
+#     for j in range(2, 21):
+#         if i % j == 0:
+#             continue
+#         else:
+#             break
+#     else:
+#         print(i)
+#         break
+# print('%s секунд' % (time.time() - start))
